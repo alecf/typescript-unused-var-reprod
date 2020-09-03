f2b163c4a71744356c310193be3b9d50f1d9160a
Alec Flett <alecf@flett.org>
2020-09-03 21:06:14 +0000

    another repro

--- index.tsx
+++ index.tsx
@@ -1,8 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-  k: infer I
-) => void
-  ? I
-  : never
-
-export type Foo = UnionToIntersection<{ foo: number } | { bar: string }>
+export interface WithArgs<EA extends any[]> {
+  aMethod(...args: EA);
+}
